33893d8ecfefb425728e2c9c336db10f47540236
fuldaros <litefros@gmail.com>
2023-06-28 14:30:42 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Отслеживаем событие клика по кнопке
+var button = document.querySelector('.button');
+button.addEventListener('click', function() {
+  // Открываем репозиторий на GitHub в новой вкладке
+  window.open('https://github.com/xyzmean', '_blank');
+});
